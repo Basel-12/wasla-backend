06209650fbf2cb62ad0ccac6be687d0d068f4bf3
Basel-12 <bassilfathy100@gmail.com>
2026-04-20 15:04:00 +0200

    add resend otp endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -59,4 +59,15 @@ export class AuthController {
     verifyOtp(@Body('userId') userId: number, @Body('otp') otp: number) {
         return this.authService.verifyOtp(userId, otp);
     }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('/resend-otp')
+    @Version('1')
+    async resendOtp(@Body('userId') userId: number, @I18nLang() lang: string) {
+        await this.authService.sendOtp(userId, 'resend', lang);
+        return {
+            success: true,
+            message: this.i18nService.t('auth.OTPRESENT'),
+        };
+    }
 }
