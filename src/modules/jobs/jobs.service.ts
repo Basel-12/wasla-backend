c01690458ae4bf579a6f28b323e2102ebadac62f
Basel-12 <bassilfathy100@gmail.com>
2026-05-11 11:08:54 +0300

    update crown notification job time

--- src/modules/jobs/jobs.service.ts
+++ src/modules/jobs/jobs.service.ts
@@ -11,7 +11,7 @@ export class JobsService {
         private Logger: Logger,
     ) {}
 
-    @Cron(CronExpression.EVERY_DAY_AT_11AM)
+    @Cron(CronExpression.EVERY_30_MINUTES)
     async handleUserVerification() {
         this.Logger.log('Sending Welcome Notification to newly verified users');
         const users =
