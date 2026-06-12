c5d933ccfd92d2996c60a9a9ef3ef8571896b4f7
Basel-12 <bassilfathy100@gmail.com>
2026-06-12 07:38:04 +0300

    update notification to run 3very 5 seconds

--- src/modules/jobs/jobs.service.ts
+++ src/modules/jobs/jobs.service.ts
@@ -11,7 +11,7 @@ export class JobsService {
         private Logger: Logger,
     ) {}
 
-    @Cron(CronExpression.EVERY_10_MINUTES)
+    @Cron(CronExpression.EVERY_5_SECONDS)
     async handleUserVerification() {
         this.Logger.log('Sending Welcome Notification to newly verified users');
         const users =
